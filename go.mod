b10b55eade5ea2d953f0eb107756d6ac2809bef1
root <root@DESKTOP-2SF12AB>
2023-07-25 18:20:46 +0300

    boilerplate

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/rivenhl1/real-estate-platform
 
 go 1.20
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	pass := os.Getenv("MYSQL_PASSWORD")
+	fmt.Println(pass)
+}
